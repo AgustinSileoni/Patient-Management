712e2c03eb8694d351f652095a2fb66a0f60f817
AgustinSileoniUNC <agustinsileoni98@gmail.com>
2025-08-11 23:29:53 -0300

    Creado el controlador
    Se creo el endpoint para obtener todos los pacientes de la base de datos y devolver la lista de todos los patients en la base de datos wrapped en un ResponseEntity

--- patient-service/src/main/java/com/agustinsileoni/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/agustinsileoni/patientservice/controller/PatientController.java
@@ -1,9 +1,28 @@
 package com.agustinsileoni.patientservice.controller;
 
+import com.agustinsileoni.patientservice.DTO.PatientResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.agustinsileoni.patientservice.service.PatientService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
 }
